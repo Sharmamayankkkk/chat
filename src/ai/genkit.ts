b92a130294c2bba2637959b98ccdc609ff555d25
Mayank Sharma <226mayankkle@gmail.com>
2025-08-02 10:56:28 +0000

    Refer the website `https://ai.google.dev/gemini-api/docs/quickstart`, i

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,12 @@
 
 import { genkit } from 'genkit';
 import { googleAI } from '@genkit-ai/googleai';
+import { nextPlugin } from '@genkit-ai/next';
 
 export const ai = genkit({
     plugins: [
         googleAI(),
+        nextPlugin(),
     ],
     logLevel: "debug",
     enableTracingAndMetrics: true,
